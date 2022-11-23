e63a1ecb7ced93535563669fde32ed7fb2e0e181
SirenaIsmail <37363206+SirenaIsmail@users.noreply.github.com>
2022-11-23 21:49:07 +0200

    secound

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable // implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -50,5 +50,11 @@ class User extends Authenticatable
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+
+    public function files()
+    {
+        return $this->hasMany(File::class, 'file_id', 'id');
     }
 }
